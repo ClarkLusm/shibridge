af6a0ca7f3c8db7d6350a82b4b46b1d2c38e4648
Thanh Phan <phanthanh.humg@gmail.com>
2023-08-17 22:11:05 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react';
 import Header from './Header';
 import Transfer from './Transfer';
 
 function App() {
+
+  const [landscape, setLandscape] = useState(false);
+
+  useEffect(() => {
+    windowResize();
+  }, [])
+
+  window.onresize = () => {
+    windowResize();
+  }
+
+  const windowResize = () => {
+    const h = window.innerHeight,
+      w = window.innerWidth;
+    setLandscape(h > w);
+  }
+
   return (
-    <div className="main">
+    <div className={landscape ? 'landscape' : ''}>
       <Header />
       <Transfer />
     </div>
